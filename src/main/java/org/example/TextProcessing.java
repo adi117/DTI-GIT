e8eaf998a908682ba1522dfb8428b029526fd62f
adi wicaksono <muhammadiwicaksono@gmail.com>
2025-02-04 14:26:02 +0700

    add remove character feature

--- src/main/java/org/example/TextProcessing.java
+++ src/main/java/org/example/TextProcessing.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class TextProcessing {
+
+    public static String removeChar(String givenText, String searchText){
+        String[] arrayText = givenText.split(searchText);
+        return String.join("", arrayText);
+    }
 }
