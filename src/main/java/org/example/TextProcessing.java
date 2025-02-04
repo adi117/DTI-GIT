59f812d361117244711b1450c06e844a64d09b93
adi wicaksono <muhammadiwicaksono@gmail.com>
2025-02-04 14:33:11 +0700

    add palindrome checker

--- src/main/java/org/example/TextProcessing.java
+++ src/main/java/org/example/TextProcessing.java
@@ -6,4 +6,20 @@ public class TextProcessing {
         String[] arrayText = givenText.split(searchText);
         return String.join("", arrayText);
     }
+
+    public static Boolean checkPalindrome(String text){
+        String[] characterText = text.split("");
+        
+        boolean isPalindrome = false;
+        
+        for (int i = 0; i < characterText.length / 2; i++) {
+            if (characterText[i] == characterText[characterText.length - i]){
+                isPalindrome = true;
+            } else {
+                isPalindrome = false;
+            }
+        }
+        
+        return isPalindrome;
+    }
 }
