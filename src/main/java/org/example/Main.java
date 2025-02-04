4fc56702e176bf941baf5f3f860d83b19176b2a0
adi wicaksono <muhammadiwicaksono@gmail.com>
2025-02-04 15:55:12 +0700

    fix user interface

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,19 +11,27 @@ public class Main {
         System.out.print("Fahrenheit temperature : ");
         System.out.println("To Celsius : " + NumberProcessing.fahrenheitToCelsius(scanner.nextDouble()) + " C");
 
+        System.out.println("----------------------");
+
         System.out.print("Centimeter : ");
         System.out.println("To Kilometer : " + NumberProcessing.cmToKm(scanner.nextDouble()) + " Km");
+
+        System.out.println("----------------------");
 
         System.out.print("Number to check : ");
         System.out.println("Is Even : " + NumberProcessing.isEven(scanner.nextInt()));
 
         scanner.nextLine();
 
+        System.out.println("----------------------");
+
         System.out.print("Input original text : ");
         String text = scanner.nextLine();
         System.out.print("Character to be removed : ");
         String characterRemoved = scanner.nextLine();
         System.out.println("Result : " + TextProcessing.removeChar(text, characterRemoved));
+
+        System.out.println("----------------------");
 
         System.out.print("Check palindrome : ");
         System.out.print("Is palindrome : " + TextProcessing.checkPalindrome(scanner.next()));
